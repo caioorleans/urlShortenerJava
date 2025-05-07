6f3ac814ba1f92af8b88fb3967bee6188a111900
Caio Orleans <caioorleansdm@gmail.com>
2025-05-07 19:43:52 -0300

    Configured Swagger

---
+++ src/main/java/com/github/caioorleans/urlshortener/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.github.caioorleans.urlshortener.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("URL shortener")
+                        .version("v1")
+                        .description("An API to short and access URLs")
+                );
+    }
+}
